c232cfb71447fb0806b53536500368467142a910
amna <amna.haider21@gmail.com>
2017-01-29 18:38:08 +0400

    Added static todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,41 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 
 var TodoApp = React.createClass({
+	getInitialState: function(){
+		return {
+			todos:[
+			{
+				id: 1,
+				text: 'Cooking Food'
+			},
+			{
+				id: 2,
+				text: 'Cleaning House'
+			},
+			{
+				id: 3,
+				text: "Fruits cutting"
+				
+			},
+			{
+				id: 4,
+				text:'watxhing drama'
+			}
+			
+			]
+			
+		}
+		
+	},
 	
 	render : function(){
+		var {todos} = this.state;
 		return (
 		<div>
 		<h1>Todo App</h1>
+		<TodoList todos = {todos}/>
 		</div>
 		
 		);
